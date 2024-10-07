d58556c5071ec90f4dc79f2e9d022ae62180e4a1
Soumyajeet <soumyajeet75@gmail.com>
2024-10-07 14:40:10 +0530

    API Test cases

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,48 @@
 package com.example.demo;
 
+import com.example.demo.dto.customerDto.CustomerResponse;
+import com.example.demo.repositeries.CustomerRepo;
+import com.example.demo.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @SpringBootTest
+@Slf4j
+@ExtendWith(MockitoExtension.class)
 class DemoApplicationTests {
 
+	@Mock
+	private CustomerRepo repo;
+
+	@InjectMocks
+	private CustomerService customerService;
+
+	@Test
+	public void saveDetails_ShouldSaveCustomer_WhenValidRequest() {
+		// Arrange
+		CustomerResponse request = new CustomerResponse();
+		request.setCustomerName("Rajesh Doe");
+		request.setCustomerAge("30");
+		request.setCustomerProfession("Engineer");
+		request.setCustomerSalary("100000");
+
+		// Act
+		String response = customerService.saveDetails(request);
+
+		// Assert
+		verify(repo, times(1)).save(request);
+		assertEquals("Successfully updated the details of customer Rajesh Doe in DataBase", response);
+		log.info(response);
+	}
 	@Test
 	void contextLoads() {
 	}
